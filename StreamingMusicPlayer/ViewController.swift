7e25148c68331523538671c96bdce9085ab48ea9
Anton Efimenko <reloni@ya.ru>
2016-07-08 10:18:39 +0300

    Initial Commit

---
+++ StreamingMusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreamingMusicPlayer
+//
+//  Created by Anton Efimenko on 08.07.16.
+//  Copyright © 2016 Anton Efimenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
