340055b02c3f8d2aa3a1fb1052a410d4c1081c87
Anton Efimenko <reloni@ya.ru>
2016-07-10 20:02:51 +0300

    Rebuild dependent frameworks

--- Scripts/CopySubmoduleFrameworks.sh
+++
@@ -1,5 +0,0 @@
-# Copy RxHttpClient.framework, because it's used as submodule and may be changed while development
-cp -R ./XCodeBuild/Products/Debug-iphonesimulator/RxHttpClient.framework ./Dependencies/iOS
-cp -R ./XCodeBuild/Products/Debug-iphonesimulator/RxHttpClientJasonExtension.framework ./Dependencies/iOS
-cp -R ./XCodeBuild/Products/Debug-iphonesimulator/RxStreamPlayer.framework ./Dependencies/iOS
-exit 0
