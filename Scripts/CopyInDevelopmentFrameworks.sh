3220827c3da2be761f309e4612b70a793b5f81a4
Anton Efimenko <reloni@ya.ru>
2016-07-10 20:36:36 +0300

    Silent CopyInDevelopmentFrameworks.sh

--- Scripts/CopyInDevelopmentFrameworks.sh
+++ Scripts/CopyInDevelopmentFrameworks.sh
@@ -1,5 +1,5 @@
 # Copy frameworks that may be changed while development into dependencies folder, to allow Travis-CI use actual version for testing
-cp -R $1/Debug-iphonesimulator/RxHttpClient.framework ./Dependencies/iOS
-cp -R $1/Debug-iphonesimulator/RxHttpClientJasonExtension.framework ./Dependencies/iOS
-cp -R $1/Debug-iphonesimulator/RxStreamPlayer.framework ./Dependencies/iOS
+cp -R $1/Debug-iphonesimulator/RxHttpClient.framework ./Dependencies/iOS 2>/dev/null || :
+cp -R $1/Debug-iphonesimulator/RxHttpClientJasonExtension.framework ./Dependencies/iOS 2>/dev/null || :
+cp -R $1/Debug-iphonesimulator/RxStreamPlayer.framework ./Dependencies/iOS 2>/dev/null || :
 exit 0
